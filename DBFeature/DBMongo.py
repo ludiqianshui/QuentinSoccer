978f8f648bdebe23d42dc57c5e3933ee59c7d225
ludiqianshui <songqiguang@gmail.com>
2014-09-01 14:14:36 +0200

    init files

---
+++ DBFeature/DBMongo.py
@@ -0,0 +1,5 @@
+'''
+This file provides database accessibility feature, designed in MongoDB 
+'''
+#import json
+import unittest
---
+++ PurchaseStrategy/average_purchase.py
@@ -0,0 +1,3 @@
+'''
+This file provides average purchase per game, and comparing with the times to win per round  
+'''
