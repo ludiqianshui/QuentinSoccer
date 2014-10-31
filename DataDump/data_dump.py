d0f52c9fc4a358267b7209d542a4e3b5cb0d2fea
ludiqianshui <songqiguang@gmail.com>
2014-10-31 17:15:16 +0100

    add full function

--- DataDump/data_dump.py
+++ DataDump/data_dump.py
@@ -20,11 +20,11 @@ class prosoccer_data_dump(object):
     UrlTemplate= "http://www.prosoccer.gr/en/{}/{}/soccer-predictions-{}-{}-{}.html"
     
     GameInfoDic = {'GameTime': '', 'GameLeague':'', 'GameHost':'', 'GameGuest':'', 
-                'PredictPossibility_3': '', 'PredictPossibility_1':'','PredictPossibility_0':'', 'TIPS':'', 
-                'ODD_3':'', 'ODD_1':'', 'ODD_0':'', 
+                'PredictPossibility_1': '', 'PredictPossibility_X':'','PredictPossibility_2':'', 'PredictTIPS':'', 
+                'ODD_1':'', 'ODD_X':'', 'ODD_2':'', 
                 'PredictScore_1': '', 'PredictScore_2': '', 
                 'PredictPossibilityUnder2.5': '', 'PredictPossibilityOver2.5': '',
-                'FinalResult':'' }
+                'FinalScore':''}
     
     GameInfoList = []
     
@@ -56,19 +56,19 @@ class prosoccer_data_dump(object):
             Month = single_date.strftime("%m")
             Day = single_date.strftime("%d")
             
-            test_team_result = self.get_prosoccer_data(single_date.year, Month, Day)
+            print single_date
+            game_info = self.get_prosoccer_data(single_date.year, Month, Day)
             
-            if self.parse_prosoccer_html(test_team_result) == True:
+            if self.parse_prosoccer_html(game_info) == True:
                 print "parse data correct and saved in the DB"
                             
             if HTMLFILE_BACKUP_FLAG == True: 
                 file_name = '../UsefulData/HTMLFiles/{}_{}_{}.html'.format(single_date.year, Month, Day)
                 soccer_date_file = open(file_name, 'w')
-                soccer_date_file.write(test_team_result)
+                soccer_date_file.write(game_info)
                 soccer_date_file.close()
-                print single_date
             
-        return test_team_result
+        return game_info
 
     def parse_prosoccer_html(self, prosoccer_html_str):
         
@@ -78,7 +78,7 @@ class prosoccer_data_dump(object):
         TableContent = soup.find("table", {"id": "anyid"} ) 
         if TableContent.__str__() == "":
             return False
-        print TableContent
+#         print TableContent
         tabel_root = ET.fromstring(TableContent.__str__())
         
 # get content of tr(each tr is a each game info) and td (each td is a item of the game info)
@@ -98,50 +98,64 @@ class prosoccer_data_dump(object):
                 
                 # get finished game score 
                 if td_tag_list[14].text == ' - ':
-                    self.GameInfoDic['FinalResult'] = ''
                     self.FlagGameFinished = False
                 else: 
-                    self.GameInfoDic['FinalResult'] = td_tag_list[14].text
                     self.FlagGameFinished = True
+                    self.GameInfoDic['FinalScore'] = td_tag_list[14].text
                
+                # get tips 
+                tips_value = td_tag_list[6].getchildren()[0].tail
+                if tips_value == "emp":
+                    self.GameInfoDic['PredictTIPS'] = "NOBET"
+                elif tips_value[0] == "p":
+                    self.GameInfoDic['PredictTIPS'] = str(tips_value[1:]).upper()
+                elif tips_value[0] == "r":  #haibukeyi
+                    self.GameInfoDic['PredictTIPS'] = str(tips_value[1:]).upper()
+                elif tips_value[0] == "w":
+                    self.GameInfoDic['PredictTIPS'] = str(tips_value[1:]).upper()
+                else :
+                    self.GameInfoDic['PredictTIPS'] = "NOBET"
+                
+                # get odds
+                self.GameInfoDic['ODD_1'] = str(td_tag_list[7].text).replace('\xc2\xa0','')
+                self.GameInfoDic['ODD_X'] = str(td_tag_list[8].text).replace('\xc2\xa0','')
+                self.GameInfoDic['ODD_2'] = str(td_tag_list[9].text).replace('\xc2\xa0','')
+             
+                # get predict 2.5 Goals 
+                self.GameInfoDic['PredictPossibilityUnder2.5'] = td_tag_list[12].text 
+                self.GameInfoDic['PredictPossibilityOver2.5'] = td_tag_list[13].text 
+
                 # if the game finished      
                 if self.FlagGameFinished == True:
                     # get predict possibility win
-                    self.GameInfoDic['PredictPossibility_3'] = td_tag_list[3].text
+                    self.GameInfoDic['PredictPossibility_1'] = td_tag_list[3].text
                     # get predict possibility draw
-                    self.GameInfoDic['PredictPossibility_1'] = td_tag_list[4].text
+                    self.GameInfoDic['PredictPossibility_X'] = td_tag_list[4].text
                     # get predict possibility lose
-                    self.GameInfoDic['PredictPossibility_0'] = td_tag_list[5].text
-                    #get tips
-                    self.GameInfoDic['TIPS'] = 
-                    
-                    
+                    self.GameInfoDic['PredictPossibility_2'] = td_tag_list[5].text
+                    # get predict score             
+                    self.GameInfoDic['PredictScore_1'] = td_tag_list[10].text + td_tag_list[10].getchildren()[0].text
+                    self.GameInfoDic['PredictScore_2'] = td_tag_list[11].text + td_tag_list[11].getchildren()[0].text
+
                 # if the game not finished
                 if self.FlagGameFinished == False:
                     # get predict possibility win
                     number_1st_digit = td_tag_list[3].getchildren()[0].attrib['class'].split('_')[-1]
                     number_2st_digit = td_tag_list[3].getchildren()[1].attrib['class'].split('_')[-1]
-                    self.GameInfoDic['PredictPossibility_3'] = int(number_1st_digit + number_2st_digit)
+                    self.GameInfoDic['PredictPossibility_1'] = int(number_1st_digit + number_2st_digit)
                     # get predict possibility draw
                     number_1st_digit = td_tag_list[4].getchildren()[0].attrib['class'].split('_')[-1]
                     number_2st_digit = td_tag_list[4].getchildren()[1].attrib['class'].split('_')[-1]
-                    self.GameInfoDic['PredictPossibility_1'] = int(number_1st_digit + number_2st_digit)
+                    self.GameInfoDic['PredictPossibility_X'] = int(number_1st_digit + number_2st_digit)
                     # get predict possibility lose
                     number_1st_digit = td_tag_list[5].getchildren()[0].attrib['class'].split('_')[-1]
                     number_2st_digit = td_tag_list[5].getchildren()[1].attrib['class'].split('_')[-1]
-                    self.GameInfoDic['PredictPossibility_0'] = int(number_1st_digit + number_2st_digit)
-                    #get tips
-                    self.GameInfoDic['TIPS'] = td_tag_list[5].text
-                
-                
-                
+                    self.GameInfoDic['PredictPossibility_2'] = int(number_1st_digit + number_2st_digit)
+                    # get predict score             
+                    self.GameInfoDic['PredictScore_1'] = td_tag_list[10].getchildren()[0].text +"-" + td_tag_list[10].getchildren()[1].text
+                    self.GameInfoDic['PredictScore_2'] = td_tag_list[11].getchildren()[0].text +"-" + td_tag_list[11].getchildren()[1].text
                 
                 print self.GameInfoDic
-             
-           
-#         self.GameInfoList 
-#         self.GameInfo
-#         
         return True
         
 
@@ -157,7 +171,7 @@ class prosoccer_data_dump(object):
             RetValue = True
 
         return RetValue
-    
+
 class Test(unittest.TestCase):
 
 #     def test_team_data_by_league(self):
@@ -166,24 +180,17 @@ class Test(unittest.TestCase):
 #  
 #         print test_team_result
 #         return 
-# 
-#     def test_parse_prosoccer_html(self):
-#         pre_predictz = prosoccer_data_dump()
-#         test_team_result = pre_predictz.get_update_prosoccer_data_before_today()
-#          
-#         print test_team_result
-#         return
     
-#     def test_prosoccer_data_before_today(self):
-#         pre_predictz = prosoccer_data_dump()
-#         test_team_result = pre_predictz.get_update_prosoccer_data_before_today()
-#         
-#         print test_team_result
-#         return
-    
-    def test_prosoccer_only_today(self):
+    def test_prosoccer_data_before_today(self):
         pre_predictz = prosoccer_data_dump()
-        RetValue = pre_predictz.get_prosoccer_data_only_today()
-   
-        print RetValue
-        return
+        test_team_result = pre_predictz.get_update_prosoccer_data_before_today()
+         
+#         print test_team_result
+        return test_team_result
+# 
+#     def test_prosoccer_only_today(self):
+#         pre_predictz = prosoccer_data_dump()
+#         RetValue = pre_predictz.get_prosoccer_data_only_today()
+#     
+#         print RetValue
+#         return
