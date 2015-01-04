a1f4af771326dc78c9f2ecef379845a484eb7323
ludiqianshui <songqiguang@gmail.com>
2015-01-04 16:13:42 +0100

    adb

--- InPlayGame/InPlayBySelenium.py
+++ InPlayGame/InPlayBySelenium.py
@@ -5,12 +5,10 @@ cookiesss = browser.get_cookies()
 
 '''
 
-import time
 import unittest
 
-from bs4 import BeautifulSoup
 from selenium import webdriver
-
+from Williamhill import Williamhill
 
 GameInfo = {'GameTime': '', 'GameLeague':'', 'GameHost':'', 'GameGuest':'', 
                 'WilliamhillGameLink':'',
@@ -24,35 +22,14 @@ class InPlayGameBySelenium (object):
     
     def __init__(self):
         self.browser = webdriver.Firefox()
+        self.williamhill_instance = Williamhill()
         
     def __del__(self):
         self.browser.quit()
         
     def get_game_list (self):
 
-        self.browser.get(self.InPlayGameURL)
-        
-        self.browser.find_element_by_id("yesBtn").click()  # Get ECT time
-        time.sleep(5)
-        self.browser.find_element_by_id("oddsSelect").send_keys("Decimal")
-        self.browser.find_element_by_id("changeOrder").send_keys("Time")
-        
-        time.sleep(10)
-        page_data = self.browser.page_source.encode('ASCII', 'ignore')
-        """       
-            save the info in a html file, this part can be removed.
-        """
-        file_name = 'GameToday.html'
-        soccer_date_file = open(file_name, 'w')
-        soccer_date_file.write(page_data)
-        soccer_date_file.close()
-        
-        soup=BeautifulSoup(page_data)
-# get the table content by the given tag of anyid for the talble id
-        GameContent = soup.find_all('tr', {"class": "rowOdd" }) 
-#         GameContent1 = soup.find(id="rowOdd")
-        print len(GameContent)
-
+        self.williamhill_instance.get_game_list("rowLive")
         return 
     
     
