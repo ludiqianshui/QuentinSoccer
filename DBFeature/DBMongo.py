7b36ac6da9a0a0d9ac99e59eb7f6adf16ee79243
Quentin Song <quentinsong@Mac-Pro-van-Quentin-Song.local>
2017-01-20 21:42:47 +0100

    ss

--- DBFeature/DBMongo.py
+++
@@ -1,4 +0,0 @@
-'''
-This file provides database accessibility feature, designed in MongoDB 
-'''
-#import json
--- PurchaseStrategy/average_purchase.py
+++
@@ -1,3 +0,0 @@
-'''
-This file provides average purchase per game, and comparing with the times to win per round  
-'''
