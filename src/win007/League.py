67030e23e28b3cb4accde275956bf41d977e704f
qsong <songqiguang@gmail.com>
2017-01-23 17:51:16 +0100

    add league info

---
+++ src/win007/League.py
@@ -0,0 +1,20 @@
+"""
+    This module provides League related function
+"""
+
+import os
+
+
+class League(object):
+    """
+        This module provides Team related function
+    """
+    Win007LeagueBaseUrl = "http://zq.win007.com/cn/League/"
+
+    def __init__(self):
+        return
+
+    def __del__(self):
+        return
+
+    def get_league_
