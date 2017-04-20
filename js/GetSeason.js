ab3cfaa49d81ac4013918f58a971e95d8f5030ee
qsong <songqiguang@gmail.com>
2017-04-20 17:14:19 +0200

    add get season

--- js/GetSeason.js
+++ js/GetSeason.js
@@ -1,3 +1,14 @@
-var sourceFile = require('./season2016_2017_s8');
+var sourceFile = require('./2015-2016_11');
+console.log(sourceFile.arrLeague[0]);//to get data from league-season
 console.log(sourceFile.arrLeague[1]);
+console.log(sourceFile.arrLeague[2]);
+console.log(sourceFile.arrLeague[4]);
+console.log(sourceFile.arrLeague[3]);
 
+console.log(sourceFile.jh["R_1"][0][0]);//to get data from rounds of league-season
+console.log(sourceFile.jh["R_1"][0][1]);
+
+
+Object.keys(sourceFile.jh).forEach((keyName) => {
+	console.log(keyName, sourceFile.jh[keyName][0][0]);
+})
