d048ac47e1a0f50f260a6e501d6755bc46e6a8e1
QSong <qsong@libertyglobal.com>
2017-08-03 16:34:26 +0200

    data

---
+++ src/DataBuild/Game/GameInfo.py
@@ -0,0 +1,24 @@
+'''
+Created on 3 Aug 2017
+
+@author: qsong
+'''
+
+class GameInfo(object):
+    '''
+    This module provides League related function
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        return
+    
+    def __del__(self):
+        return
+        
+    def get_game_info_by_id (self, game_id):
+        
+    
