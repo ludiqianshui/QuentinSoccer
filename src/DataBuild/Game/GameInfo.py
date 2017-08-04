3d31a535718eb16b59a164196ec5bac9834a868a
QSong <qsong@libertyglobal.com>
2017-08-04 11:33:44 +0200

    up

--- src/DataBuild/Game/GameInfo.py
+++ src/DataBuild/Game/GameInfo.py
@@ -3,14 +3,17 @@ Created on 3 Aug 2017
 
 @author: qsong
 '''
+import unittest
+import urllib2
+from bs4 import BeautifulSoup
 
 class GameInfo(object):
     '''
     This module provides League related function
     '''
-
-
-    def __init__(self, params):
+    game_base_url = "http://zq.win007.com/analysis/{}.htm"
+        
+    def __init__(self):
         '''
         Constructor
         '''
@@ -20,5 +23,29 @@ class GameInfo(object):
         return
         
     def get_game_info_by_id (self, game_id):
+        game_info_url = self.game_base_url.format(game_id)
+        url_data = urllib2.urlopen(game_info_url)
+        soup = BeautifulSoup(url_data)
+        odd_table_body = soup.find_all("script")
+        print odd_table_body    
         
+        return
     
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.test_obj = GameInfo()
+
+    def tearDown(self):
+        self.test_obj = None
+        return
+
+    def test_get_game_info(self):
+        self.test_obj.get_game_info_by_id(1365286)
+        # self.test_obj.get_goldenbet()
+        return
+
+
+        
+        
